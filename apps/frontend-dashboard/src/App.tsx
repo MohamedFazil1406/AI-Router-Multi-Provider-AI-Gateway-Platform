fe3c23154827effc9a459102259ac0178ae32378
MohamedFazil1406 <mohamedfazil01406@gmail.com>
2026-04-29 11:42:14 +0530

    All done till need to  implement razor pay

---
+++ apps/frontend-dashboard/src/App.tsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/Landing";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Dashboard } from "./pages/Dashboard";
+import { ApiKeys } from "./pages/ApiKeys";
+import { Credits } from "./pages/Credits";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { treaty } from "@elysia/eden";
+import { ElysiaClientContextProvider } from "./provider/Eden";
+import type { App } from "app";
+import "./index.css";
+
+const client = treaty<App>(`http://localhost:3000`, {
+  fetch: {
+    credentials: "include",
+  },
+});
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ElysiaClientContextProvider value={client}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/api-keys" element={<ApiKeys />} />
+            <Route path="/credits" element={<Credits />} />
+          </Routes>
+        </BrowserRouter>
+      </ElysiaClientContextProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
